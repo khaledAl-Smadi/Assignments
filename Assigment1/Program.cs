4e9a6dc4ad3d0be95a58ef619e2189fb16326e4b
Khalid Al-Samadi <sriheen20@gmail.com>
2025-10-26 21:27:23 +0300

    First commit

---
+++ Assigment1/Program.cs
@@ -0,0 +1,30 @@
+﻿Console.WriteLine("Enter the number of days [1 - 7]: ");
+int days = Convert.ToInt32(Console.ReadLine());
+
+switch (days)
+{
+    case 1:
+        Console.WriteLine("Sunday");
+        break;
+    case 2:
+        Console.WriteLine("Monday");
+        break;
+    case 3:
+        Console.WriteLine("Tuesday");
+        break;
+    case 4:
+        Console.WriteLine("Wednesday");
+        break;
+    case 5:
+        Console.WriteLine("Thursday");
+        break;
+    case 6:
+        Console.WriteLine("Friday");
+        break;
+    case 7:
+        Console.WriteLine("Saturday");
+        break;
+    default:
+        Console.WriteLine("Invalid number");
+        break;
+}
---
+++ Assignment 2/Program.cs
@@ -0,0 +1,42 @@
+﻿Console.WriteLine("Enter The number Month [1 - 12]");
+int Month = Convert.ToInt32(Console.ReadLine());
+switch(Month)
+{
+    case 1: Console.WriteLine($"The Month {Month} => (January) => 31 days");
+        break;
+    case 2:
+        Console.WriteLine($"The Month {Month} => (February) => 28 days");
+        break;
+    case 3:
+        Console.WriteLine($"The Month {Month} => (March) => 31 days");
+        break;
+    case 4:
+        Console.WriteLine($"The Month {Month} => (April) => 30 days");
+        break;
+    case 5:
+        Console.WriteLine($"The Month {Month} => (May) => 31 days");
+        break;
+    case 6:
+        Console.WriteLine($"The Month {Month} => (June) => 30 days");
+        break;
+    case 7:
+        Console.WriteLine($"The Month {Month} => (July) => 31 days");
+        break;
+    case 8:
+        Console.WriteLine($"The Month {Month} => (August) => 31 days");
+        break;
+    case 9:
+        Console.WriteLine($"The Month {Month} => (September) => 30 days");
+        break;
+    case 10:
+        Console.WriteLine($"The Month {Month} => (October) => 31 days");
+        break;
+    case 11:
+        Console.WriteLine($"The Month {Month} => (November) => 30 days");
+        break;
+    case 12:
+        Console.WriteLine($"The Month {Month} => (December) => 31 days");
+        break;
+    default: Console.WriteLine("Invalid Number");
+        break;
+}
