1a280be6b10771e924331a561fc9341094aa3d96
khaledAl-Smadi <khaledalsmadi003@gmail.com>
2025-10-31 18:56:29 +0300

    Add files via upload

---
+++ Assignment5/Program.cs
@@ -0,0 +1,23 @@
+﻿Dictionary<char, int> stringCounts = new Dictionary<char, int>()
+{ };
+Console.WriteLine("Enter a word:");
+string Word = Console.ReadLine();
+foreach (var item in Word)
+{
+    if (stringCounts.ContainsKey(item))
+    {
+        stringCounts[item]++;
+    }
+    else
+    {
+        stringCounts[item] = 1;
+    }
+}
+foreach (var item in Word)
+{
+    if (stringCounts[item] == 1)
+    {
+        Console.WriteLine($"The first non repeating is ( {item} )");
+        break;
+    }
+}
