78721659025514de19c2d12132c0117456f1509c
Khalid Al-Samadi <sriheen20@gmail.com>
2025-10-31 16:51:08 +0300

    Yes<https://github.com/khaledAl-Smadi/Assignments.git>

--- Assigment1/obj/Release/net9.0/Assigment1.AssemblyInfo.cs
+++ Assigment1/obj/Release/net9.0/Assigment1.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("Assigment1")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4e9a6dc4ad3d0be95a58ef619e2189fb16326e4b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Assigment1")]
---
+++ Assignment3/Program.cs
@@ -0,0 +1,19 @@
+﻿float[] Mark = { 47.5f, 55, 80, 90, 33.2f, 35 };
+float Sum = 0.0f;
+
+for (int i = 0; i < Mark.Length; i++)
+{
+    Sum += Mark[i];
+}
+
+float Avg = Sum / Mark.Length;
+Console.WriteLine($"The Average is: {Avg:F2}\n");
+
+Console.WriteLine("Numbers greater than the average:");
+for (int i = 0; i < Mark.Length; i++)
+{
+    if (Mark[i] > Avg)
+    {
+        Console.WriteLine(Mark[i]);
+    }
+}
